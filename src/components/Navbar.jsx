81b60cbcda2a77e35586c0a9bd27128dd968d20d
mahin67580 <mahin67580@gmail.com>
2025-09-26 16:04:09 +0600

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -169,18 +169,18 @@ export default function Navbar() {
                                     </Avatar>
                                 </Button>
                             </DropdownMenuTrigger>
-                            <DropdownMenuContent className="w-56" align="end" forceMount>
+                            <DropdownMenuContent className="w-56  " align="end" forceMount>
                                 <DropdownMenuLabel className="font-normal">
                                     <div className="flex flex-col space-y-1">
-                                        <p className="text-sm font-medium leading-none">{session.user.name}</p>
-                                        <p className="text-xs leading-none text-muted-foreground">
+                                        <p className="text-xl font-medium leading-none">{session.user.name}</p>
+                                        <p className="  leading-none text-muted-foreground">
                                             {session.user.email}
                                         </p>
                                     </div>
                                 </DropdownMenuLabel>
                                 <DropdownMenuSeparator />
                                 <DropdownMenuItem asChild>
-                                    <Link href="/profile">Profile</Link>
+                                    <Link href="/profile" >Profile</Link>
                                 </DropdownMenuItem>
 
                                 {/* Role-based Dashboard */}
